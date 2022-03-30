d2ad8a8a24d73563812e430cf712a12d2ccb5404
kunchao322 <chao.k@northeastern.edu>
2022-03-30 16:03:26 -0400

    fixed cast error by monitory cookie secure

--- server.ts
+++ server.ts
@@ -52,7 +52,7 @@ let sess = {
     resave: true,
     cookie: {
         sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production" ? true : false,
     }
 }
 
